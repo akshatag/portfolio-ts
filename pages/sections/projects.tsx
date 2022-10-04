9e169738c47a19dfc5a0be4cd2eb91aa34354a06
Akshat Agrawal <akshat@Akshats-MacBook-Pro.local>
2022-10-03 20:10:46 -0400

    added Zenn to projects

--- pages/sections/projects.tsx
+++ pages/sections/projects.tsx
@@ -23,6 +23,7 @@ const Projects = () => {
                 PROJECTS
             </Heading>
             <SimpleGrid p={15} columns={{sm: 1, md: 2}} spacing={10}>
+                <ProjectTile bg="blue.400" name="Zenn" link="https://zennjournal.vercel.app"></ProjectTile>
                 <ProjectTile bg="tomato" name="NFTrees" link="https://github.com/akshatag/nftrees"></ProjectTile>
                 <ProjectTile bg="teal" name="Tetris3D" link="https://github.com/akshatag/tetris-3d"></ProjectTile>
                 <ProjectTile bg="blue.400" name="Pickup" link="https://devpost.com/software/pickup-0qtovx"></ProjectTile>
